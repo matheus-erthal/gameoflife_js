8c05582de758469c90287ecfab3383e47b59f8cf
Matheus Erthal <matheusamancio@id.uff.br>
2021-05-07 03:09:47 -0300

    Adicionando comentários

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
+// imports necessários para ler linhas e ler arquivos
 const readline = require("readline");
 const fs = require('fs')
 
+// abrindo a interface para ler do terminal
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
+// interface principal com o usuário, na qual ele escolhe o modo de uso do programa
 rl.question("Bem vindo ao Jogo da Vida!\nEscolha entre o modo por arquivo(A) ou modo randômico(R): ", function(modo) {
     rl.question("Digite o número de iterações máxima: ", function(i) {
         if(modo == "A"){
             modo_arquivo(rl, i);
         }else if(modo == "R"){
             modo_randomico(rl, i);
+        }else{
+            console.warn("O modo não foi reconhecido.");
+            rl.close();
         }
     }); 
 });
 
+// no modo arquivo, o usuário fornecerá um arquivo(entrada.txt) com uma grade de números,
+// com colunas separadas por espaços e linhas separadas por quebras de linha
 const modo_arquivo = (rl, i) => {
     try {
         const lines = fs.readFileSync('entrada.txt').toString().split("\n");
@@ -28,6 +36,7 @@ const modo_arquivo = (rl, i) => {
     }
 }
 
+// no modo randômico, o usuário escolhe a quantidade de linha e de colunas da grade
 const modo_randomico = (rl, i) => {
     rl.question("Digite o número de linhas: ", function(n_linhas) {
         rl.question("Digite o número de colunas: ", function(n_colunas) {
@@ -37,8 +46,10 @@ const modo_randomico = (rl, i) => {
     });
 }
 
-const passo = grade => grade.map((linha, i) => linha.map((celula, j) => proximo_estado(grade, i, j)));
+// essa função recebe a grade como parâmetro, calcula o próximo estado de cada elemento, e retorna uma nova grade
+const passo = grade => grade.map((linha, i) => linha.map((celula, j) => proximo_estado(grade, grade[i][j], 0, 0, 0, i, j, i-1, j-1)));
 
+// essa função efetivamente retorna o próximo estado baseado na quantidade de células vivas e zumbis adjacentes
 const transforma_estado = (atual, vivos, zumbis) => {
     if(atual == 0 && vivos == 3) return 1;
     if(atual == 1 && zumbis > 0) return 2;
@@ -48,11 +59,13 @@ const transforma_estado = (atual, vivos, zumbis) => {
     return atual;
 }
 
-const proximo_estado_rec = (grade, atual, vivos, mortos, zumbis, i_inicial, j_inicial, i, j) => {
+// essa função, por meio de suas chamadas recursivas, percorre todos os vizinhos da célula
+// atual e retorna qual será o próximo estado
+const proximo_estado = (grade, atual, vivos, mortos, zumbis, i_inicial, j_inicial, i, j) => {
     if(i > i_inicial+1 || i == grade.length) {
         return transforma_estado(atual, vivos, zumbis);
     }
-    if(i < 0) return proximo_estado_rec(
+    if(i < 0) return proximo_estado(
         grade, 
         atual, 
         vivos, 
@@ -63,7 +76,7 @@ const proximo_estado_rec = (grade, atual, vivos, mortos, zumbis, i_inicial, j_in
         i+1, 
         j_inicial-1
     );
-    if(j < 0) return proximo_estado_rec(
+    if(j < 0) return proximo_estado(
         grade, 
         atual, 
         vivos, 
@@ -74,7 +87,7 @@ const proximo_estado_rec = (grade, atual, vivos, mortos, zumbis, i_inicial, j_in
         i, 
         j+1
     );
-    if(j == grade[0].length) return proximo_estado_rec(
+    if(j == grade[0].length) return proximo_estado(
         grade, 
         atual, 
         vivos, 
@@ -85,7 +98,7 @@ const proximo_estado_rec = (grade, atual, vivos, mortos, zumbis, i_inicial, j_in
         i + 1, 
         j_inicial - 1
     );
-    if(j == j_inicial && i == i_inicial) return proximo_estado_rec(
+    if(j == j_inicial && i == i_inicial) return proximo_estado(
         grade, 
         atual, 
         vivos, 
@@ -96,7 +109,7 @@ const proximo_estado_rec = (grade, atual, vivos, mortos, zumbis, i_inicial, j_in
         i, 
         j+1
     );
-    if(j > j_inicial) return proximo_estado_rec(
+    if(j > j_inicial) return proximo_estado(
         grade, 
         atual, 
         grade[i][j] == 1 ? vivos+1 : vivos, 
@@ -107,7 +120,7 @@ const proximo_estado_rec = (grade, atual, vivos, mortos, zumbis, i_inicial, j_in
         i+1, 
         j_inicial-1
     );
-    else return proximo_estado_rec(
+    else return proximo_estado(
         grade, 
         atual, 
         grade[i][j] == 1 ? vivos+1 : vivos, 
@@ -120,16 +133,14 @@ const proximo_estado_rec = (grade, atual, vivos, mortos, zumbis, i_inicial, j_in
     );
 }
 
-const proximo_estado = (grade, i, j) => {
-    const atual = grade[i][j];
-    return proximo_estado_rec(grade, atual, 0, 0, 0, i, j, i-1, j-1);
-
-}
-
+// essa função verifica se uma linha é igual a outra
 const equal_list = (lis1, lis2) => !lis1.map((elemento, index) => lis2[index] == elemento).includes(false)
 
+// essa função verifica se uma grade é igual a outra
 const equal_grade = (grade, grade2) => !grade.map((elemento, index) => equal_list(elemento, grade2[index]) ).includes(false)
 
+// essa função realiza efetivamente as iterações na grade, parando ou quando o sistema
+// estabiliza, ou quando o número máximo de iterações foi alcançado
 const iterador = (grade_atual, grade_anterior, max_iteracoes, iteracao_atual) => {
     if(iteracao_atual == max_iteracoes){
         console.warn(grade_atual);
@@ -144,18 +155,23 @@ const iterador = (grade_atual, grade_anterior, max_iteracoes, iteracao_atual) =>
     return iterador(passo(grade_atual), grade_atual, max_iteracoes, iteracao_atual+1);
 }
 
+// essa função centraliza a lógica do modo randômico
 const jogo_da_vida_randomico = (linhas, colunas, i) => {
     const estado_inicial = inicializa_grade(linhas, colunas);
     console.warn(estado_inicial);
     console.warn(`${iterador(estado_inicial, null, i, 0)} iterações`);
 }
 
+// essa função gera um número aleatório, dentro de um certo intervalo. OBS: função impura.
 const gera_aleatorio = (min, max) => ~~(Math.random() * (max - min + 1)) + min;
 
+// essa função inicializa a grade com i linhas e j colunas e valores aleatórios
 const inicializa_grade = (i, j) => [...Array(parseInt(i))].map(() => [...Array(parseInt(j))].map(() => inicializa_ponto()))
 
+// essa função inicializa um ponto aleatório
 const inicializa_ponto = () => gera_aleatorio(0, 2);
 
+// esse callback serve para encerrar o programa da forma correta
 rl.on("close", function() {
     console.log("\nFim do programa");
     process.exit(0);
